ef0a3d3468fddcb65512e220996effbaf628877c
ArtyomEW <152199050+ArtyomEW@users.noreply.github.com>
2024-03-25 00:46:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,43 @@ import time
 links = numpy.array([])
 
 
+def taking_email_phone_and_website(link):
+    global links
+    fn = '2gis.xlsx'
+    wb = load_workbook(fn)
+    ws = wb['Лист1']
+
+    """Функция что бы браузер не закрывался"""
+    chr_options = Options()
+    chr_options.add_experimental_option("detach", True)
+    driver = webdriver.Chrome(options=chr_options)
+
+    """Переходим на наш сайт"""
+    driver.get(link)
+    time.sleep(3)
+    bs = BeautifulSoup(driver.page_source, 'html.parser')
+    mail_and_site = bs.findAll('div', class_='_49kxlr')
+    name_company = bs.findAll('h1', class_='_tvxwjf')
+    lst = []
+    for i in name_company:
+        lst.append(i.text[:-1])
+    for i in mail_and_site:
+        if i.find('a', class_='_1rehek') is not None:
+            if 'ru' in i.text or 'com' in i.text:
+                lst.append(i.text)
+        if i.find('a', class_='_2lcm958') is not None:
+            if 'ru' in i.text or 'com' in i.text:
+                lst.append(i.text)
+    link = driver.find_elements(By.TAG_NAME, 'a')
+    for teg in link:
+        phone = teg.get_attribute('href')
+        if phone is not None and 'tel:' in phone:
+            lst.append(phone)
+    ws.append(lst)
+    wb.save(fn)
+    driver.quit()
+
+
 def add_el_in_links(svg, driver):
     global links
 
@@ -26,6 +63,8 @@ def add_el_in_links(svg, driver):
         '''Кликаем по тегу для перехода на следующую страницу'''
         driver.execute_script("arguments[0].click();", svg)
     driver.quit()
+    for i in links:
+        taking_email_phone_and_website(i)
 
 
 def main(link: str):
@@ -49,5 +88,7 @@ def main(link: str):
 
 
 if __name__ == '__main__':
-    main('https://2gis.ru/spb/search/%D0%B8%D0%BD%D0%B6%D0%B5%D0%BD%D0%B5%D1%80%D0%BD%D0%B0%D1%8F%20%D1%81%D0%B0%D0'
-         '%BD%D1%82%D0%B5%D1%85%D0%BD%D0%B8%D0%BA%D0%B0?m=30.313803%2C59.944294%2F11')
+    main('https://2gis.ru/spb/search/%D0%A1%D0%B8%D1%81%D1%82%D0%B5%D0%BC%D1%8B%20%D0%BE%D1%82%D0%BE%D0%BF%D0%BB%D0'
+         '%B5%D0%BD%D0%B8%D1%8F%20%2F%20%D0%B2%D0%BE%D0%B4%D0%BE%D1%81%D0%BD%D0%B0%D0%B1%D0%B6%D0%B5%D0%BD%D0%B8%D1'
+         '%8F%20%2F%20%D0%BA%D0%B0%D0%BD%D0%B0%D0%BB%D0%B8%D0%B7%D0%B0%D1%86%D0%B8%D0%B8/rubricId/489?m=30.314838'
+         '%2C59.938138%2F15.96')
