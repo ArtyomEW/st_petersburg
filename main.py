8fc1c82e86857feaa3adf723cded2f4fcc5a6e61
ArtyomEW <152199050+ArtyomEW@users.noreply.github.com>
2024-03-24 22:39:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -51,8 +51,3 @@ def main(link: str):
 if __name__ == '__main__':
     main('https://2gis.ru/spb/search/%D0%B8%D0%BD%D0%B6%D0%B5%D0%BD%D0%B5%D1%80%D0%BD%D0%B0%D1%8F%20%D1%81%D0%B0%D0'
          '%BD%D1%82%D0%B5%D1%85%D0%BD%D0%B8%D0%BA%D0%B0?m=30.313803%2C59.944294%2F11')
-    # taking_email_phone_and_website('https://2gis.ru/spb/search/%D0%A1%D0%B8%D1%81%D1%82%D0%B5%D0%BC%D1%8B%20%D0%BE%D1'
-    #                                '%82%D0%BE%D0%BF%D0%BB%D0%B5%D0%BD%D0%B8%D1%8F%20%2F%20%D0%B2%D0%BE%D0%B4%D0%BE%D1'
-    #                                '%81%D0%BD%D0%B0%D0%B1%D0%B6%D0%B5%D0%BD%D0%B8%D1%8F%20%2F%20%D0%BA%D0%B0%D0%BD%D0'
-    #                                '%B0%D0%BB%D0%B8%D0%B7%D0%B0%D1%86%D0%B8%D0%B8/rubricId/489/firm/70000001069094676'
-    #                                '?m=30.314838%2C59.938138%2F15.96')
